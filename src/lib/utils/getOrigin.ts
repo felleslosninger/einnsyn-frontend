2c28bb8c9333f5a0cd0df69e90be6a5f5adb770c
Gisle Nes <gisle@gisle.net>
2025-06-25 11:34:03 +0200

    Use 'x-forwarded-host' instead of 'host'

--- src/lib/utils/getOrigin.ts
+++ src/lib/utils/getOrigin.ts
@@ -4,7 +4,7 @@ import { headers } from 'next/headers';
 
 export async function getOrigin() {
   const headersList = await headers();
-  const host = headersList.get('host');
+  const host = headersList.get('x-forwarded-host') || headersList.get('host');
   const protocol = headersList.get('x-forwarded-proto') || 'http';
 
   return `${protocol}://${host}`;
