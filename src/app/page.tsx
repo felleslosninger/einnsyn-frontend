f6a7e1d2c6c2dbc793376fda5029a55b851ccd5d
Gisle Nes <gisle@gisle.net>
2026-02-05 12:53:07 +0100

    Remove nested containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
-import { notFound, redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import { cachedApiClient } from '~/actions/api/getApiClient';
 import { cachedAuthInfo } from '~/actions/authentication/auth';
+import ApiKeyLogin from '~/features/admin/api-keys/ApiKeyLogin';
 import { logger } from '~/lib/utils/logger';
 import ApiKeys from '../features/admin/api-keys/ApiKeys';
-import { AnsattportenLogin } from '~/features/login/AnsattportenLogin';
-import ApiKeyLogin from '~/features/admin/api-keys/ApiKeyLogin';
 
 export default async function Root() {
   const authInfo = await cachedAuthInfo();
   if (!authInfo) {
-    return (
-      <div className="container-wrapper" >
-        <div className="container-pre" />
-        <div className="container">
-          <ApiKeyLogin />
-        </div>
-        <div className="container-post" />
-      </div>
-    );
+    return <ApiKeyLogin />;
   }
-  const apiClient = await cachedApiClient();
 
+  const apiClient = await cachedApiClient();
   const enhetId = authInfo.orgnummer ? authInfo.orgnummer : '';
   const apiKeys = await apiClient.enhet.listApiKey(enhetId).catch((error) => {
     logger.warn('Failed to fetch API keys for enhet', {
@@ -29,13 +20,6 @@ export default async function Root() {
     });
     notFound();
   });
-  return (
-    <div className="container-wrapper" >
-      <div className="container-pre" />
-      <div className="container">
-        <ApiKeys apiKeys={apiKeys} />
-      </div>
-      <div className="container-post" />
-    </div>
-  );
+
+  return <ApiKeys apiKeys={apiKeys} />;
 }
