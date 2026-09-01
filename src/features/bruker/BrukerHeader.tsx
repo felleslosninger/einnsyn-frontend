6627e261c7c4a9cb5ff135739a312e907221aaa1
Inerud <stine.vik.letrud@digdir.no>
2026-09-01 09:09:20 +0200

    Change button to heading

--- src/features/bruker/BrukerHeader.tsx
+++ src/features/bruker/BrukerHeader.tsx
@@ -5,24 +5,16 @@ import { EinButton } from '~/components/EinButton/EinButton';
 import { useTranslation } from '~/hooks/useTranslation';
 import styles from './BrukerHeader.module.scss';
 import BrukerTabs from './BrukerTabs';
+import { Heading } from '@digdir/designsystemet-react';
 
 export default function BrukerHeader() {
   const t = useTranslation();
 
   return (
     <>
-      <EinButton
-        asChild
-        data-size="sm"
-        data-color="neutral"
-        variant="tertiary"
-        className={styles.navigationLink}
-      >
-        <a href="/search">
-          <ArrowLeftIcon aria-hidden="true" />
-          {t('bruker.backToSearch')}
-        </a>
-      </EinButton>
+      <Heading className={styles.heading} data-size="sm">
+        {t('bruker.title')}
+      </Heading>
       <BrukerTabs />
     </>
   );
