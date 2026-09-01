6ff631a801b7b92fb98f5e4824557d0a7525c6e9
Gisle Nes <gisle@gisle.net>
2026-09-01 13:22:55 +0200

    chore: remove redundant typeNameMapper.ts

--- src/lib/utils/typeNameMapper.ts
+++
@@ -1,6 +0,0 @@
-import { useTranslation } from '~/hooks/useTranslation';
-
-export function mapJournalpostType(type: string): string {
-  const t = useTranslation();
-  return t(`journalpost.type.${type}`);
-}
